e86358c1ae171f3eed1fe31cf33c15e53d6a3fa4
Riponz <riponbiswasfilms@gmail.com>
2023-05-22 09:41:01 +0530

    welcome animation 1.2

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -5,26 +5,23 @@ import { useNavigate } from "react-router-dom";
 function Welcome() {
   const navigate = useNavigate();
   useEffect(() => {
-  //   const delayGo = () => {
-        
-  //   }
-
-
-  setTimeout(function() {
-    navigate('/')
-  }, 4000);
+    setTimeout(function () {
+      navigate("/");
+    }, 4000);
   }, []);
 
   return (
     <div className="welcome">
-      <span className="welcome-span">lets confess!</span>
-      <svg className="svg" viewBox="0 0 1000 400">
-        <text id="mytext" x="50%" y="50%" text-anchor="middle" fill="none">
-          confessionity
-        </text>
-        <use xlink:href="#mytext" class="copy copy1"></use>
-        <use xlink:href="#mytext" class="copy copy2"></use>
-      </svg>
+      <div className="welcome-container">
+        <svg className="svg" viewBox="0 0 1000 400">
+          <text id="mytext" x="50%" y="50%" text-anchor="middle" fill="none">
+            confessionity
+          </text>
+          <use xlink:href="#mytext" class="copy copy1"></use>
+          <use xlink:href="#mytext" class="copy copy2"></use>
+        </svg>
+        <span className="welcome-span">lets confess!</span>
+      </div>
     </div>
   );
 }
